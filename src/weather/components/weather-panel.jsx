4ad568c6625ba8ca9141d44937c48d4e2a6a471b
Collin McLean <collin@mclean.codes>
2020-11-30 00:19:48 -0500

    Style detailing

--- src/weather/components/weather-panel.jsx
+++ src/weather/components/weather-panel.jsx
@@ -13,22 +13,22 @@ export default function WeatherPanel ({loading, weatherData, zipCode, zipCodeCha
 
     return (
         <div className="weather-panel">
-            <span className="d-flex justify-content-center">
-                <h2 className="text-center">{weatherData?.cityName || '...'}</h2>
-                <img className="weather-icon" crossOrigin="https://openweathermap.org/img/wn" src={weatherData?.iconURL} alt={weatherData?.description} />
+            <span className="d-flex justify-content-center align-items-center">
+                <h2 className="text-center m-0">{weatherData?.cityName || '...'}</h2>
+                <img className="weather-icon" crossOrigin="https://openweathermap.org/" src={weatherData?.iconURL} alt={weatherData?.description} />
             </span>
-            <h3 className="text-center text-capitalize">{weatherData?.description || '...'}</h3>
-            <h1 className="text-center mt-5">{Math.round(weatherData?.temp || 0)} ˚</h1>
-            <span className="d-flex mt-2 justify-content-center">
+            <h4 className="text-center text-capitalize">{weatherData?.description || '...'}</h4>
+            <h1 className="text-center ml-4 current-temp">{Math.round(weatherData?.temp || 0)}˚</h1>
+            <span className="d-flex mt-3 justify-content-center">
                 <h4 className="mr-4">{Math.round(weatherData?.tempMin || 0)} ˚</h4>
                 <h4 className="d-inline">{Math.round(weatherData?.tempMax || 0)} ˚</h4>
             </span>
             <hr className="my-5" />
             <form onSubmit={onSubmit} className="form-group">
-                <label htmlFor="zipCode">Zip Code</label>
+                <label htmlFor="zipCode">Zip Code:</label>
                 <span className="d-flex">
-                    <input className="form-control mr-2" name="zipCode" value={zipCode} onChange={onChange} />
-                    <button className="form-control btn btn-primary" type="submit">Update</button>
+                    <input className="form-control input-zip-code mr-2" name="zipCode" value={zipCode} onChange={onChange} />
+                    <button className="form-control btn btn-zip-code" type="submit">Update</button>
                 </span>
             </form>
         </div>
