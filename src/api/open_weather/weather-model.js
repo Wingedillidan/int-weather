c7d2b07c7e1b317a2285776622d0163e5ee68ad1
Collin McLean <collin@mclean.codes>
2020-11-29 22:52:49 -0500

    Add weather icon

--- src/api/open_weather/weather-model.js
+++ src/api/open_weather/weather-model.js
@@ -1,3 +1,5 @@
+const IMAGE_CDN = 'https://openweathermap.org/img/wn'
+
 export default class WeatherModel {
     constructor({main, weather, name}) {
         this.cityName = name
@@ -5,5 +7,7 @@ export default class WeatherModel {
         this.temp = main?.temp
         this.tempMin = main?.temp_min
         this.tempMax = main?.temp_max
+        this.icon = weather[0]?.icon
+        this.iconURL = `${IMAGE_CDN}/${weather[0]?.icon}@2x.png`
     }
 }
--- src/weather/components/weather-panel.jsx
+++ src/weather/components/weather-panel.jsx
@@ -1,3 +1,5 @@
+import './weather-panel.scss'
+
 export default function WeatherPanel ({loading, weatherData, zipCode, zipCodeChange, getWeather}) {
     const onChange = (e) => {
         e.preventDefault()
@@ -10,8 +12,11 @@ export default function WeatherPanel ({loading, weatherData, zipCode, zipCodeCha
     }
 
     return (
-        <div>
-            <h2 className="text-center">{weatherData?.cityName || '...'}</h2>
+        <div className="weather-panel">
+            <span className="d-flex justify-content-center">
+                <h2 className="text-center">{weatherData?.cityName || '...'}</h2>
+                <img className="weather-icon" crossOrigin="https://openweathermap.org/img/wn" src={weatherData?.iconURL} alt={weatherData?.description} />
+            </span>
             <h3 className="text-center text-capitalize">{weatherData?.description || '...'}</h3>
             <h1 className="text-center mt-5">{Math.round(weatherData?.temp || 0)} ˚</h1>
             <span className="d-flex mt-2 justify-content-center">
@@ -19,7 +24,7 @@ export default function WeatherPanel ({loading, weatherData, zipCode, zipCodeCha
                 <h4 className="d-inline">{Math.round(weatherData?.tempMax || 0)} ˚</h4>
             </span>
             <hr className="my-5" />
-            <form onSubmit={onSubmit} class="form-group">
+            <form onSubmit={onSubmit} className="form-group">
                 <label htmlFor="zipCode">Zip Code</label>
                 <span className="d-flex">
                     <input className="form-control mr-2" name="zipCode" value={zipCode} onChange={onChange} />
